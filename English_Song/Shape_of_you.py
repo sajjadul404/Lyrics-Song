9e21a6ad216cf560ad908b512874184581061383
Sajjadul Islam <sajjadul625@gmail.com>
2026-03-20 00:31:13 +0600

    Shape_of_you

---
+++ English_Song/Shape_of_you.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+lyrics = [
+    "I'm in love with the shape of you",
+    "We push and pull like a magnet do",
+    "Although my heart is falling too",
+    "I'm in love with your body",
+    "And last night you were in my room",
+    "And now my bedsheets smell like you",
+    "Every day discovering something brand new",
+    "I'm in love with your body",
+    "(Oh--I--oh--I--oh--I--oh--I)",
+    "I'm in love with your body",
+    "(Oh--I--oh--I--oh--I--oh--I)",
+    "I'm in love with your body",
+    "(Oh--I--oh--I--oh--I--oh--I)",
+    "I'm in love with your body",
+    "Every day discovering something brand new",
+    "I'm in love with the shape of you",
+
+]
+
+for line in lyrics:
+    for char in line:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print()
