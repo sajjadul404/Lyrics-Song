fcc8612bea7b224169d2183d5dafff1bf1e0e48d
Sajjadul Islam <sajjadul625@gmail.com>
2026-03-27 12:21:42 +0600

    Space All lines

--- Hindi_Song/Tera_Zikr.py
+++ Hindi_Song/Tera_Zikr.py
@@ -4,15 +4,15 @@ import time
 # 0:50 minute Start Lyrics
 print("\n\033[34m🎧 Now Playing: Tera Zikr - Darshan Raval\033[0m\n")
 lyrics = [
-    "Mujhe khone ke baad ik din",
-    "Tum mujhe yaad karooooge   ",
-    "Phir dekhna milne ki mujhse",
-    "Tum fariyad karoooooge ....\n",
+    "   Mujhe khone ke baad ik din",
+    "   Tum mujhe yaad karooooge   ",
+    "   Phir dekhna milne ki mujhse",
+    "   Tum fariyad karoooooge ....\n",
 
-    "Mujhe khone ke baad ik din",
-    "Tum mujhe yaad karooooge   ",
-    "Phir dekhna milne ki mujhse",
-    "Tum fariyad karoooooge .... \n",
+    "   Mujhe khone ke baad ik din",
+    "   Tum mujhe yaad karooooge   ",
+    "   Phir dekhna milne ki mujhse",
+    "   Tum fariyad karoooooge .... \n",
 ]
 
 for line in lyrics:
