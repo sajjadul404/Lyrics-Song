55dbcb4f01d6804614939b3b063eadf66275009d
Sajjadul Islam <sajjadul625@gmail.com>
2026-05-08 23:10:39 +0600

    Hale dil

---
+++ Hale_Dil.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+# 0:50 minute Start Lyrics
+print("\n\033[34m🎧 Now Playing: Tera Zikr - Darshan Raval\033[0m\n")
+lyrics = [
+    
+"Haal e dil tujhko sunata",
+"Dil agar yeh bol paata",
+"Bakhuda tujhko hai chahta jaa aaa aaa aan\n",
+
+"Tere sang jo pal bitaata",
+"Waqt se main woh maang lata",
+"Yaad karke muskuraata haan",
+"Woo….\n"
+]
+
+for line in lyrics:
+    for char in line:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.14)
+    print()
